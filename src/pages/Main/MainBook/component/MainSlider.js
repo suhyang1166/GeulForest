a0df457e685ed17e504cd6405eaa43f27e376e5d
suhyang1166 <yooyootea1166@gmail.com>
2024-05-13 16:25:16 +0900

    feat: api 취합본

--- src/pages/Main/MainBook/component/MainSlider.js
+++ src/pages/Main/MainBook/component/MainSlider.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { styled } from "styled-components";
+
+// const MainBg
 
 const MainSlider = ({ bestseller }) => {
-    // console.log(bestseller);
-    return (
-        <div>
-            <div
-                style={{
-                    backgroundImage: `url(${bestseller?.cover})`,
-                    width: "200px",
-                    height: "280px",
-                }}
-            ></div>
-            <p>{bestseller?.title}</p>
-        </div>
-    );
+  console.log(bestseller);
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${bestseller?.cover})`,
+          width: "200px",
+          height: "280px",
+        }}
+      ></div>
+      <p>{bestseller?.title}</p>
+    </div>
+  );
 };
 
 export default MainSlider;
